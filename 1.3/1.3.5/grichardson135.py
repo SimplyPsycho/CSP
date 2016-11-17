754729aab94232a8b9a76b305b1df5d3059b5d08
SimplyPsycho <g.richardson@bssdstd.net>
2016-11-17 13:22:31 -0600

    1.3.5

---
+++ 1.3/1.3.5/grichardson135.py
@@ -0,0 +1,10 @@
+def how_eligible(essay):
+    number = 0
+    if '?' in essay:
+        number = number + 1
+    else:
+        
+    if '"' in essay:
+        number = number + 1
+    else:
+        number = number + 0
